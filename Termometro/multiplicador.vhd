615e3383e79e57af483e01539851932a325c915e
Mixthor <miguel.a.gvb@gmail.com>
2025-05-09 14:39:15 +0200

    Multiplicador existe y update interpreter

--- Termometro/multiplicador.vhd
+++ Termometro/multiplicador.vhd
@@ -8,23 +8,22 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 
-entity input_interpreter is
+entity multiplicador is
     generic (
         C_SAMPLE_FREQ   : integer := 125 --000000;
     );
     Port ( clk_i    :   in std_logic;
            rst_i    :   in std_logic;
-           start_i  :   in std_logic;
-           drdy_in  :   in std_logic;
+           rdy_in  :   in std_logic;
            n_in     :   std_logic_vector(15 downto 0);
            n_out    :   std_logic_vector(15 downto 0)
     );
 
-end input_interpreter;
+end multiplicador;
 
-architecture Behavioral of input_interpreter is
+architecture Behavioral of multiplicador is
 
-    type Status_g is (S_IDLE, S_WAIT, S_MULT1);
+    type Status_g is (S_IDLE, S_MULT1);
     signal STATE        :   Status_g;
 
     signal n_celsius    :   std_logic_vector(15 downto 0);
@@ -44,19 +43,13 @@ begin
 
                 when S_IDLE =>
 
-                    if (start_i = '1') then
-                        STATE <= S_WAIT;
-                    end if;
-
-                when S_WAIT =>
-
-                    if (drdy_in = '1') then
+                    if (rdy_in = '1') then
                         STATE <= S_MULT1;
                     end if;
                     
                 when S_MULT1 =>
 
-                    STATE <= S_WAIT;
+                    STATE <= S_IDLE;
 
             end case;
         end if;
